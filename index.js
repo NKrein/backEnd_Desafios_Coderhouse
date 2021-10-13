7f3872a833eb1340a67b4a2612639d5d1526ed52
Nicolás Krein Werle <nikokrein@gmail.com>
2021-10-13 20:40:15 -0300

    Correccion Desafio 3: Muestra datos con servidor Express

--- index.js
+++ index.js
@@ -21,21 +21,37 @@ app.get('/', (req, res) => {
 `)
 })
 
-app.get('/productos', (req, res) => {
+app.get('/productos', (require, response) => {
   const products = new Container('./file.txt');
   const allProducts = products.getAll();
-  res.send(allProducts.then(res => {
-    console.log('Todos los productos ->', res);
-  }).catch(err => console.log('Error ->', err)));
+  allProducts.then( res => {
+    response.send(`  
+    <div style="display: flex; flex-flow: row wrap;">
+      ${res.map(e => `
+        <div style='text-align: center; margin: 50px;'>
+          <h3>${e.title}</h3>
+          <img width='100px' src=${e.thumbnail} alt=${e.title}>
+          <p><b>$${e.price}</b></p>
+          <p>Id: ${e.id}</p>
+        </div>
+      `)}
+    </div>`
+  )}).catch(err => console.log('Error ->', err));
 })
 
-app.get('/productoRandom', (req, res) => {
+app.get('/productoRandom', (require, response) => {
   const products = new Container('./file.txt');
   const allProducts = products.getAll();
-  res.send(allProducts.then(res => {
-    const n = random.int(0, res.length-1);
-    console.log('Producto random ->', res[n]);
-  }).catch(err => console.log('Error ->', err)));
+  allProducts.then(res => {
+  const n = random.int(0, res.length - 1);
+  response.send(`
+    <div style='text-align: center; margin: 50px;'>
+      <h3>${res[n].title}</h3>
+      <img width='100px' src=${res[n].thumbnail} alt=${res[n].title}>
+      <p><b>$${res[n].price}</b></p>
+      <p>Id: ${res[n].id}</p>
+    </div>`
+  )}).catch(err => console.log('Error ->', err));
 })
 
 const server = app.listen(PORT, () => {
